d028730c5a738a6e0b738c0cd5b59c50262dc09d
xiangtianyu <xiangtianyu@zhongan.com>
2023-10-12 09:40:44 +0800

    loading spiiner

--- src/main/java/com/zhongan/codeai/gui/toolwindows/chat/CodeAIChatToolWindow.java
+++ src/main/java/com/zhongan/codeai/gui/toolwindows/chat/CodeAIChatToolWindow.java
@@ -4,12 +4,15 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ui.componentsList.components.ScrollablePanel;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.ui.components.JBScrollPane;
+import com.intellij.util.ui.AsyncProcessIcon;
+import com.intellij.util.ui.JBUI;
 import com.zhongan.codeai.gui.toolwindows.components.ChatDisplayPanel;
 import com.zhongan.codeai.gui.toolwindows.components.UserChatPanel;
 import com.zhongan.codeai.integrations.llms.LlmProviderFactory;
 import com.zhongan.codeai.integrations.llms.entity.CodeAIChatCompletionRequest;
 import com.zhongan.codeai.integrations.llms.entity.CodeAIMessage;
 
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.util.List;
@@ -28,15 +31,21 @@ public class CodeAIChatToolWindow {
 
     private final Project project;
 
+    private final AsyncProcessIcon loadingSpinner;
+
     public JPanel getCodeAIChatToolWindowPanel() {
         return codeAIChatToolWindowPanel;
     }
 
     public CodeAIChatToolWindow(Project project, ToolWindow toolWindow) {
         this.project = project;
+        this.loadingSpinner = new AsyncProcessIcon("network-waiting-spinner");
         this.codeAIChatToolWindowPanel = new JPanel(new GridBagLayout());
         this.chatContentPanel = new ScrollablePanel();
         this.userChatPanel = new UserChatPanel(this::syncSendAndDisplay);
+
+        loadingSpinner.setBorder(JBUI.Borders.emptyLeft(8));
+        loadingSpinner.setVisible(false);
 
         var gbc = new GridBagConstraints();
         gbc.fill = GridBagConstraints.BOTH;
@@ -59,7 +68,7 @@ public class CodeAIChatToolWindow {
         codeAIChatToolWindowPanel.add(userChatPanel, gbc);
     }
 
-    private void showChatContent(String content) {
+    private void showChatContent(String content, int type) {
         var gbc = new GridBagConstraints();
         gbc.fill = GridBagConstraints.BOTH;
         gbc.weighty = 0;
@@ -71,6 +80,16 @@ public class CodeAIChatToolWindow {
         text.setLayout(new BoxLayout(text, BoxLayout.PAGE_AXIS));
         text.setText(content);
         text.setEditable(false);
+
+        // response should show loading
+        if (type == 1) {
+            var panel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+            panel.setBorder(JBUI.Borders.empty());
+            loadingSpinner.resume();
+            loadingSpinner.setVisible(true);
+            panel.add(loadingSpinner);
+            chatContentPanel.add(panel);
+        }
 
         ChatDisplayPanel chatDisplayPanel = new ChatDisplayPanel().setText(text);
         chatContentPanel.add(chatDisplayPanel, gbc);
@@ -91,12 +110,13 @@ public class CodeAIChatToolWindow {
 
     private void syncSendAndDisplay(String message) {
         // show prompt
-        showChatContent(message);
+        showChatContent(message, 0);
 
         // FIXME
         new Thread(() -> {
             String result = sendMessage(this.project, message);
-            showChatContent(result);
+            showChatContent(result, 1);
+            loadingSpinner.setVisible(false);
         }).start();
     }
 }
