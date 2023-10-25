d5168b74c84471f441dfdbb908b2c8794d968775
张志生 <zhangzhisheng@zhongan.com>
2023-10-25 10:16:57 +0800

    Feature/fix multi idea window show

--- src/main/java/com/zhongan/codeai/gui/toolwindows/CodeAIChatToolWindowFactory.java
+++ src/main/java/com/zhongan/codeai/gui/toolwindows/CodeAIChatToolWindowFactory.java
@@ -1,5 +1,6 @@
 package com.zhongan.codeai.gui.toolwindows;
 
+import com.google.common.collect.Maps;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
@@ -7,15 +8,18 @@ import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import com.zhongan.codeai.gui.toolwindows.chat.CodeAIChatToolWindow;
 
+import java.util.Map;
+
 import org.jetbrains.annotations.NotNull;
 
 public class CodeAIChatToolWindowFactory implements ToolWindowFactory {
-    public static CodeAIChatToolWindow codeAIChatToolWindow = null;
+
+    private static Map<Project, CodeAIChatToolWindow> codeAIChatToolWindows = Maps.newConcurrentMap();
 
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-        codeAIChatToolWindow = new CodeAIChatToolWindow(project, toolWindow);
-
+        CodeAIChatToolWindow codeAIChatToolWindow = new CodeAIChatToolWindow(project, toolWindow);
+        codeAIChatToolWindows.put(project, codeAIChatToolWindow);
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
 
         Content content = contentFactory.createContent(codeAIChatToolWindow.getCodeAIChatToolWindowPanel(), "", false);
@@ -23,4 +27,7 @@ public class CodeAIChatToolWindowFactory implements ToolWindowFactory {
         toolWindow.getContentManager().addContent(content);
     }
 
+    public static CodeAIChatToolWindow getCodeAIChatToolWindow(@NotNull Project project) {
+        return codeAIChatToolWindows.get(project);
+    }
 }
--- src/main/java/com/zhongan/codeai/util/DocumentUtil.java
+++ src/main/java/com/zhongan/codeai/util/DocumentUtil.java
@@ -1,6 +1,7 @@
 package com.zhongan.codeai.util;
 
 import com.google.common.collect.Lists;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
@@ -37,18 +38,18 @@ public class DocumentUtil {
      */
     public static void insertCommentAndFormat(Project project, Editor editor, String result) {
         //insert comment, do not check result have code block
-        WriteCommandAction.runWriteCommandAction(project, () -> {
+        ApplicationManager.getApplication().invokeLater(() -> WriteCommandAction.runWriteCommandAction(project, () -> {
             Document document = editor.getDocument();
             int caretOffset = editor.getSelectionModel().getSelectionStart();
             document.replaceString(editor.getSelectionModel().getSelectionStart(),
-                editor.getSelectionModel().getSelectionEnd(),
-                result);
+                    editor.getSelectionModel().getSelectionEnd(),
+                    result);
             // format code
             CodeStyleManager codeStyleManager = CodeStyleManager.getInstance(project);
             VirtualFile file = FileDocumentManager.getInstance().getFile(editor.getDocument());
             codeStyleManager.reformatText(PsiManager.getInstance(project).findFile(file),
-                caretOffset, caretOffset + result.length());
-        });
+                    caretOffset, caretOffset + result.length());
+        }));
     }
 
     public static boolean containsCode(String content) {
