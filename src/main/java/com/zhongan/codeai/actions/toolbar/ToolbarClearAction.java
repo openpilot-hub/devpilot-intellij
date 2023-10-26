f25f3d2ede3c12a43bceada365fb4d990e765cd1
肖亮 <xiaoliang@zhongan.com>
2023-10-26 17:18:17 +0800

    update welcome panel

--- src/main/java/com/zhongan/codeai/actions/toolbar/ToolbarClearAction.java
+++ src/main/java/com/zhongan/codeai/actions/toolbar/ToolbarClearAction.java
@@ -4,12 +4,15 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.zhongan.codeai.CodeAIIcons;
 import com.zhongan.codeai.gui.toolwindows.CodeAIChatToolWindowFactory;
+import com.zhongan.codeai.util.CodeAIMessageBundle;
 
 import org.jetbrains.annotations.NotNull;
 
 public class ToolbarClearAction extends AnAction {
     public ToolbarClearAction() {
-        super(CodeAIIcons.CLEAR_ICON);
+        super(CodeAIMessageBundle.get("codeai.toolbarClearAction.text"),
+                CodeAIMessageBundle.get("codeai.toolbarClearAction.text"),
+                CodeAIIcons.CLEAR_ICON);
     }
 
     @Override
