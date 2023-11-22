1fad56ae0f78f6e312720e7a3d1c80f2a837db3c
肖亮 <xiaoliang@zhongan.com>
2023-11-22 11:23:44 +0800

    fix source code jump

--- src/main/java/com/zhongan/devpilot/util/EditorUtils.java
+++ src/main/java/com/zhongan/devpilot/util/EditorUtils.java
@@ -9,31 +9,30 @@ import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.Balloon;
-import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileManager;
 
 import org.jetbrains.annotations.NotNull;
 
 public class EditorUtils {
 
-    public static void openFileAndSelectLines(@NotNull Project project, String filePath, int startLine, int endLine) {
-        if (!FileUtil.exists(filePath)) {
+    public static void openFileAndSelectLines(@NotNull Project project, String fileUrl, int startLine, int endLine) {
+
+        VirtualFile codeFile = VirtualFileManager.getInstance().findFileByUrl(fileUrl);
+        if (codeFile == null || !codeFile.exists()) {
             BalloonAlertUtils.showErrorAlert(DevPilotMessageBundle.get("devpilot.alter.file.not.exist"), 0, -10, Balloon.Position.above);
+            return;
         }
 
-        VirtualFile file = LocalFileSystem.getInstance().findFileByPath(filePath);
-        if (file != null) {
-            Editor editor = FileEditorManager.getInstance(project).openTextEditor(new OpenFileDescriptor(project, file), true);
-            if (editor != null) {
-                SelectionModel selectionModel = editor.getSelectionModel();
-                int startOffset = editor.getDocument().getLineStartOffset(startLine - 1);
-                int endOffset = editor.getDocument().getLineEndOffset(endLine - 1);
-                selectionModel.setSelection(startOffset, endOffset);
+        Editor editor = FileEditorManager.getInstance(project).openTextEditor(new OpenFileDescriptor(project, codeFile), true);
+        if (editor != null) {
+            SelectionModel selectionModel = editor.getSelectionModel();
+            int startOffset = editor.getDocument().getLineStartOffset(startLine - 1);
+            int endOffset = editor.getDocument().getLineEndOffset(endLine - 1);
+            selectionModel.setSelection(startOffset, endOffset);
 
-                ScrollingModel scrollingModel = editor.getScrollingModel();
-                scrollingModel.scrollTo(new LogicalPosition(startLine, 0), ScrollType.CENTER);
-            }
+            ScrollingModel scrollingModel = editor.getScrollingModel();
+            scrollingModel.scrollTo(new LogicalPosition(startLine, 0), ScrollType.CENTER);
         }
     }
 }
