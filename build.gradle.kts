5cae6b34bf6afae3c43139b2a26418e5d7eaeb2f
zhangzhisheng <zhangzhisheng@zhongan.com>
2024-03-12 14:03:01 +0800

    code completion trigger rule update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.zhongan"
-version = "2.3.0"
+version = "2.3.1"
 
 repositories {
     mavenCentral()
--- src/main/java/com/zhongan/devpilot/completions/inline/CompletionUtils.java
+++ src/main/java/com/zhongan/devpilot/completions/inline/CompletionUtils.java
@@ -1,7 +1,9 @@
 package com.zhongan.devpilot.completions.inline;
 
 import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.util.TextRange;
+import com.zhongan.devpilot.util.CommentUtil;
 
 import java.util.regex.Pattern;
 
@@ -13,9 +15,9 @@ public class CompletionUtils {
 
         String addedText = document.getText(new TextRange(previousOffset, newOffset));
         return
-            isValidMidlinePosition(document, newOffset) &&
-                isValidNonEmptyChange(addedText.length(), addedText) &&
-                isSingleCharNonWhitespaceChange(addedText);
+                isValidMidlinePosition(document, newOffset) &&
+                        isValidNonEmptyChange(addedText.length(), addedText) &&
+                        isSingleCharNonWhitespaceChange(addedText);
     }
 
     public static boolean isValidMidlinePosition(Document document, int offset) {
@@ -33,4 +35,45 @@ public class CompletionUtils {
     public static boolean isSingleCharNonWhitespaceChange(String newText) {
         return newText.trim().length() <= 1;
     }
+
+    //代码-单行仅空字符或者仅换行忽略请求，注释-单行仅换行忽略请求
+    public static boolean ignoreTrigger(String newText, String previousLineText) {
+        boolean preLineIsPreComment = CommentUtil.containsComment(previousLineText);
+        //contains empty and tab
+        boolean emptyAndTabChar = newText.trim().length() < 1 ;
+        if (emptyAndTabChar && !preLineIsPreComment) return true;
+
+        boolean newlineChar = newText.equals("\n");
+        if (newlineChar) return true;
+
+        return false;
+    }
+
+    public static boolean ignoreChange(Editor editor, Document document, int newOffset, int previousOffset) {
+        if (newOffset < 0 || previousOffset > newOffset) return false;
+        String addedText = document.getText(new TextRange(previousOffset, newOffset));
+        String escapedText = addedText.replace("\t", "\\t");
+        System.out.println(escapedText);
+
+        int currentLine = editor.getCaretModel().getLogicalPosition().line;
+        int previousLine = currentLine - 1;
+        String previousLineText = previousLine < 0 ? null : document.getText(
+                new TextRange(document.getLineStartOffset(previousLine), document.getLineEndOffset(previousLine)));
+        return
+                isValidMidlinePosition(document, newOffset) &&
+                        isValidNonEmptyChange(addedText.length(), addedText) &&
+                        isSingleCharNonWhitespaceChange(addedText) &&
+                        ignoreTrigger(addedText, previousLineText);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
