218a92e086041fab33df717738ff2941dd53e730
xiangtianyu <tjjxiang@gmail.com>
2024-08-26 11:36:52 +0800

    change encoding to utf8

--- src/main/java/com/zhongan/devpilot/util/Base64Utils.java
+++ src/main/java/com/zhongan/devpilot/util/Base64Utils.java
@@ -1,13 +1,14 @@
 package com.zhongan.devpilot.util;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class Base64Utils {
     public static String base64Encoding(String code) {
-        return Base64.getEncoder().encodeToString(code.getBytes());
+        return Base64.getEncoder().encodeToString(code.getBytes(StandardCharsets.UTF_8));
     }
 
     public static String base64Decoding(String code) {
-        return new String(Base64.getDecoder().decode(code));
+        return new String(Base64.getDecoder().decode(code), StandardCharsets.UTF_8);
     }
 }
