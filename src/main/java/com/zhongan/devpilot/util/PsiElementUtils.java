20b43db7e208f83f3e313946044b730079acd075
maozhen <58839004+maozhen520@users.noreply.github.com>
2024-09-25 17:21:34 +0800

    Inner class resolve.

--- src/main/java/com/zhongan/devpilot/util/PsiElementUtils.java
+++ src/main/java/com/zhongan/devpilot/util/PsiElementUtils.java
@@ -39,7 +39,6 @@ import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.jetbrains.annotations.NotNull;
